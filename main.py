5e837fcf3e8bead3b08c12a60f0660d54d84345c
hetshah13 <hetshah1309@gmail.com>
2024-09-22 23:32:33 +0530

    4th commit

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ we have create virtual environment: myenv
 # Libraries
 import uvicorn # for ASGI
 from fastapi import FastAPI
+import pickle
+import pandas as pd
 
 # Creating Object
 app = FastAPI()
+pickle_open = open("classifier.pkl","rb")
+classifier = pickle.load(pickle_open)
 
 # Index route, opens automatically on http://127.0.0.1:8000/
 @app.get('/')
@@ -23,6 +27,9 @@ def index():
 def get_name(name: str):
     return {'Welcome To This Web Page ' f'{name}'}
 
+@app.post('/predict')
+def predict_note(data:Banknote):
+    data = data.dict()
 
 # Run the API with uvicorn
 # It will run on http://127.0.0.1:8000
