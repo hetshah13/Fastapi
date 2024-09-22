65e05ec39919b78c4f4ad253e9f7a23abe020da6
hetshah13 <hetshah1309@gmail.com>
2024-09-22 16:34:36 +0530

    3rd commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ we have create virtual environment: myenv
 1. python -m venv myenv
 2. myenv\Scripts\activate
 3. then install libraries and dependances
+# pip install uvicorn, fastapi, uvloop
 4. deactivate (to deactivate virtual environment)
 """
 
