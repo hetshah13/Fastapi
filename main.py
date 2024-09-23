a45c34eb59cd752ca95f08b7c3d548949460d4b5
hetshah13 <hetshah1309@gmail.com>
2024-09-23 18:57:32 +0530

    update commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ we have create virtual environment: myenv
 1. python -m venv myenv
 2. myenv\Scripts\activate
 3. then install libraries and dependances
-# pip install uvicorn, fastapi, uvloop
+# pip install uvicorn, fastapi, scikit-learn, pandas
 4. deactivate (to deactivate virtual environment)
 """
 
